1f3bd7ddc40de93e7b27da261aed6ecdd6e27f10
matos <tomislavmatosevic@outlook.com>
2023-06-30 00:46:02 +0200

    Created popup window for products, created style for ProductionList.tsx

--- storefront/src/components/ProductList.tsx
+++ storefront/src/components/ProductList.tsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
+import "./ProductList.css";
 import { Product } from "../interfaces";
 import { BsFillCartPlusFill } from "react-icons/bs";
 import { MdProductionQuantityLimits } from "react-icons/md";
 import axios from "axios";
+import Popup from "reactjs-popup";
 
 function ProductLists(): JSX.Element {
   const [productList, setProductList] = useState<Product[]>([]);
   const API_URL = "http://localhost:9000";
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null); // Updated type to Product | null
 
   useEffect(() => {
     loadProducts();
@@ -21,60 +24,41 @@ function ProductLists(): JSX.Element {
     }
   }
 
+  const openModal = (product: Product) => {
+    setSelectedProduct(product);
+  };
+
+  const closeModal = () => {
+    setSelectedProduct(null);
+  };
+
   return (
     <>
       <div className="container text-center text-light">
         <h1>
           <MdProductionQuantityLimits />
-          Product Lists
+          Products List
         </h1>
-        <h1>hajduk</h1>
       </div>
 
       <div className="wrapper">
         <div className="container">
           <div className="row">
-            {productList.map((prodList: Product, key: number) => (
-              <div className="col-md-3" key={key}>
+            {productList.map((product: Product, key: number) => (
+              <div
+                className="col-md-3"
+                key={key}
+                onClick={() => openModal(product)} 
+              >
                 <div className="product-list">
                   <div className="product-image">
                     <img
-                      src={prodList.thumbnail}
-                      alt={prodList.title}
+                      src={product.thumbnail}
+                      alt={product.title}
                       className="img-responsive"
+                      height={350}
+                      width={350}
                     />
-                  </div>
-                  <div className="product-handle">
-                    <div className="category">
-                      <span>{prodList.handle}</span>
-                    </div>
-                    <div className="product-title">
-                      <h3>{prodList.title}</h3>
-                    </div>
-                    <div className="product-description">
-                      <p>{prodList.description}</p>
-                    </div>
-                    <div className="card-footer">
-                      <div className="product-price">
-                        <span className="price">
-                          {prodList.variants.map((variant: any) => (
-                            <React.Fragment key={variant.id}>
-                              {variant.prices.map((price: any) => (
-                                <React.Fragment key={price.id}>
-                                  {price.currency_code}: $
-                                  {price.amount / variant.inventory_quantity}
-                                </React.Fragment>
-                              ))}
-                            </React.Fragment>
-                          ))}
-                        </span>
-                      </div>
-                      <div className="product-cart">
-                        <a href="#" className="product-buy-btn">
-                          <BsFillCartPlusFill />
-                        </a>
-                      </div>
-                    </div>
                   </div>
                 </div>
               </div>
@@ -82,8 +66,60 @@ function ProductLists(): JSX.Element {
           </div>
         </div>
       </div>
+
+      <Popup
+         className="popup-content" 
+         open={selectedProduct !== null}
+         onClose={closeModal}
+        
+      >
+        {selectedProduct && (
+          <div className="image-container">
+            <img
+                      src={selectedProduct.thumbnail}
+                      alt={selectedProduct.title}
+                      className="img-responsive"
+                      height={350}
+                      width={350}
+                    />
+            <div className="product-handle">
+              <div className="category">
+                <span>Category: {selectedProduct.handle}</span>
+              </div>
+              <div className="product-title">
+                <h3>{selectedProduct.title}</h3>
+              </div>
+              <div className="product-description">
+                <p>{selectedProduct.description}</p>
+              </div>
+              <div className="card-footer">
+                <div className="product-price">
+                  <span className="price">
+                    {selectedProduct.variants.map((variant: any) => (
+                      <React.Fragment key={variant.id}>
+                        {variant.prices.map((price: any) => (
+                          <React.Fragment key={price.id}>
+                            {price.currency_code}: $
+                            {price.amount / variant.inventory_quantity}
+                          </React.Fragment>
+                        ))}
+                      </React.Fragment>
+                    ))}
+                  </span>
+                </div>
+                <div className="product-cart">
+                  <a href="#" className="product-buy-btn">
+                    <BsFillCartPlusFill />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </Popup>
     </>
   );
 }
 
 export default ProductLists;
+
