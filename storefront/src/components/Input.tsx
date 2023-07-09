320e12359f1a7697348b80ee3f4ea1174dc94094
matos <tomislavmatosevic@outlook.com>
2023-07-09 19:37:52 +0200

    Added filtering, search products and improved style.

--- storefront/src/components/Input.tsx
+++ storefront/src/components/Input.tsx
@@ -1,8 +1,112 @@
+import React, { useState } from "react";
+import Card from 'react-bootstrap/Card';
+import axios from "axios";
 
-function Input(){
-    return(
-        <>
-        </>
-    )
+function Input(props) {
+  const [formData, setFormData] = useState({
+    handle: "",
+    title: "",
+    description: "",
+    thumbnail: "",
+    variants: "",
+  });
+
+  const sendData = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    const forSend = processData(formData);
+
+    const API_URL = "http://localhost:9000";
+
+    axios
+      .post(`${API_URL}/store/products`, forSend)
+      .then((rez) => {
+        props.add([...props.state, rez.data]);
+      });
+  };
+
+  function processData(object) {
+    return {
+      id: "",
+      handle: object.handle,
+      title: object.title,
+      description: object.description,
+      thumbnail: object.thumbnail,
+      variants: object.variants,
+    };
+  }
+
+  function changeInput(event) {
+    const { name, value, type, checked } = event.target;
+    const newValue = type === "checkbox" ? checked : value;
+    setFormData({ ...formData, [name]: newValue });
+  }
+
+  return (
+    <>
+      <Card className="input-card">
+        <h2>Input Product</h2>
+        <form onSubmit={sendData}>
+          <label>
+            Handle:
+            <input
+              className="handle"
+              type="text"
+              name="handle"
+              value={formData.handle}
+              onChange={changeInput}
+              required
+            />
+          </label>
+          <label>
+            Title:
+            <input
+              className="title"
+              type="text"
+              name="title"
+              value={formData.title}
+              onChange={changeInput}
+              required
+            />
+          </label>
+          <label>
+            Description:
+            <input
+              className="description"
+              type="text"
+              name="description"
+              value={formData.description}
+              onChange={changeInput}
+              required
+            />
+          </label>
+          <label>
+            Thumbnail:
+            <input
+              className="thumbnail"
+              type="url"
+              name="thumbnail"
+              value={formData.thumbnail}
+              onChange={changeInput}
+              required
+            />
+          </label>
+          <label>
+            Variants:
+            <input
+              className="variants"
+              type="number"
+              name="variants"
+              value={formData.variants}
+              onChange={changeInput}
+              required
+            />
+          </label>
+          <button id="add-product" type="submit">Add Product</button>
+        </form>
+      </Card>
+    </>
+  );
 }
+
 export default Input;
--- storefront/src/components/ProductList.tsx
+++ storefront/src/components/ProductList.tsx
@@ -9,7 +9,11 @@ import Popup from "reactjs-popup";
 function ProductLists(): JSX.Element {
   const [productList, setProductList] = useState<Product[]>([]);
   const API_URL = "http://localhost:9000";
-  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null); // Updated type to Product | null
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+  const [typeFilter, setTypeFilter] = useState<string[]>([]);
+  const [statusFilter, setStatusFilter] = useState<string[]>([]);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [isEditing, setIsEditing] = useState(false);
 
   useEffect(() => {
     loadProducts();
@@ -24,6 +28,50 @@ function ProductLists(): JSX.Element {
     }
   }
 
+  const handleTypeFilterChange = (value: string) => {
+    if (typeFilter.includes(value)) {
+      setTypeFilter(typeFilter.filter((type) => type !== value));
+    } else {
+      setTypeFilter([...typeFilter, value]);
+    }
+  };
+
+  const handleStatusFilterChange = (value: string) => {
+    if (statusFilter.includes(value)) {
+      setStatusFilter(statusFilter.filter((status) => status !== value));
+    } else {
+      setStatusFilter([...statusFilter, value]);
+    }
+  };
+
+  const filterProducts = (products: Product[]) => {
+    return products
+      .filter((product) => {
+        if (typeFilter.length > 0) {
+          return typeFilter.includes(product.handle?.toLowerCase() || "");
+        }
+        return true;
+      })
+      .filter((product) => {
+        if (statusFilter.length > 0) {
+          return statusFilter.includes(
+            product.status === "published" ? "published" : "unpublished"
+          );
+        }
+        return true;
+      })
+      .filter((product) => {
+        if (searchQuery !== "") {
+          return (
+            product.title?.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            false
+          );
+        }
+        return true;
+      });
+  };
+  
+
   const openModal = (product: Product) => {
     setSelectedProduct(product);
   };
@@ -31,6 +79,20 @@ function ProductLists(): JSX.Element {
   const closeModal = () => {
     setSelectedProduct(null);
   };
+
+  const filteredProducts = filterProducts(productList);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleEditZatvori = () => {
+    setIsEditing(false);
+  };
+  interface Product {
+    // Other properties...
+    selectedVariant: any | null;
+  }
 
   return (
     <>
@@ -40,15 +102,99 @@ function ProductLists(): JSX.Element {
           Products List
         </h1>
       </div>
+      <div className="filtar">
+        <div>
+          <label>Type:</label>
+          <div>
+            <label>
+              <input
+                type="checkbox"
+                value="hoodie"
+                checked={typeFilter.includes("hoodie")}
+                onChange={(e) => handleTypeFilterChange(e.target.value)}
+              />
+              Hoodie
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                value="coffee-mug"
+                checked={typeFilter.includes("coffee-mug")}
+                onChange={(e) => handleTypeFilterChange(e.target.value)}
+              />
+              Coffee mug
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                value="shorts"
+                checked={typeFilter.includes("shorts")}
+                onChange={(e) => handleTypeFilterChange(e.target.value)}
+              />
+              Shorts
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                value="sweatpants"
+                checked={typeFilter.includes("sweatpants")}
+                onChange={(e) => handleTypeFilterChange(e.target.value)}
+              />
+              Sweatpants
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                value="t-shirt"
+                checked={typeFilter.includes("t-shirt")}
+                onChange={(e) => handleTypeFilterChange(e.target.value)}
+              />
+              T-shirt
+            </label>
+          </div>
+        </div>
+        <div>
+          <label>Status:</label>
+          <div>
+            <label>
+              <input
+                type="checkbox"
+                value="dostupno"
+                checked={statusFilter.includes("dostupno")}
+                onChange={(e) => handleStatusFilterChange(e.target.value)}
+              />
+              Dostupno
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                value="nedostupno"
+                checked={statusFilter.includes("nedostupno")}
+                onChange={(e) => handleStatusFilterChange(e.target.value)}
+              />
+              Nedostupno
+            </label>
+          </div>
+        </div>
+        <div>
+          <label htmlFor="searchQuery">Traži:</label>
+          <input
+            id="searchQuery"
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        </div>
+      </div>
 
       <div className="wrapper">
         <div className="container">
           <div className="row">
-            {productList.map((product: Product, key: number) => (
+            {filteredProducts.map((product: Product, key: number) => (
               <div
                 className="col-md-3"
                 key={key}
-                onClick={() => openModal(product)} 
+                onClick={() => openModal(product)}
               >
                 <div className="product-list">
                   <div className="product-image">
@@ -68,20 +214,42 @@ function ProductLists(): JSX.Element {
       </div>
 
       <Popup
-         className="popup-content" 
-         open={selectedProduct !== null}
-         onClose={closeModal}
-        
+        className="popup-content"
+        open={selectedProduct !== null}
+        onClose={closeModal}
       >
         {selectedProduct && (
           <div className="image-container">
             <img
-                      src={selectedProduct.thumbnail}
-                      alt={selectedProduct.title}
-                      className="img-responsive"
-                      height={350}
-                      width={350}
-                    />
+              src={selectedProduct.thumbnail}
+              alt={selectedProduct.title}
+              className="img-responsive"
+              height={350}
+              width={350}
+            />
+            <div className="product-variants">
+      <label>Select Variant:</label>
+      <select
+        value={selectedProduct.selectedVariant}
+        onChange={(e) => {
+          const variantId = e.target.value;
+          const selectedVariant = selectedProduct.variants.find(
+            (variant: any) => variant.id === variantId
+          );
+          setSelectedProduct({
+            ...selectedProduct,
+            selectedVariant,
+          });
+        }}
+      >
+        <option value="">Choose variant</option>
+        {selectedProduct.variants.map((variant: any) => (
+          <option key={variant.id} value={variant.id}>
+            {variant.name}
+          </option>
+        ))}
+      </select>
+    </div>
             <div className="product-handle">
               <div className="category">
                 <span>Category: {selectedProduct.handle}</span>
@@ -93,20 +261,17 @@ function ProductLists(): JSX.Element {
                 <p>{selectedProduct.description}</p>
               </div>
               <div className="card-footer">
-                <div className="product-price">
-                  <span className="price">
-                    {selectedProduct.variants.map((variant: any) => (
-                      <React.Fragment key={variant.id}>
-                        {variant.prices.map((price: any) => (
-                          <React.Fragment key={price.id}>
-                            {price.currency_code}: $
-                            {price.amount / variant.inventory_quantity}
-                          </React.Fragment>
-                        ))}
-                      </React.Fragment>
-                    ))}
-                  </span>
-                </div>
+              <div className="product-price">
+  {selectedProduct.selectedVariant && (
+    <span className="price">
+      {selectedProduct.selectedVariant.prices.map((price: any) => (
+        <React.Fragment key={price.id}>
+          {price.currency_code}: ${price.amount}
+        </React.Fragment>
+      ))}
+    </span>
+  )}
+</div>
                 <div className="product-cart">
                   <a href="#" className="product-buy-btn">
                     <BsFillCartPlusFill />
